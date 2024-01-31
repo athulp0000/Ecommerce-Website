236c41cb513cd49a8a5302838009bfc4d1577ed5
Athul <62958311+athulp0000@users.noreply.github.com>
2024-01-31 23:11:31 +0530

    Add files via upload

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            
+        </div>
+        <div className="descriptionbox-description">
+            <p>A website that allows people to buy and sell physical goods, services, and digital products over the internet rather than at a brick-and-mortar location. Through an e-commerce website, a business can process orders, accept payments, manage shipping and logistics, and provide customer service.</p>
+            <p>
+            Ecommerce website design involves creating and launching a virtual store to sell products. We're not talking about listing your items on a third-party online
+            </p>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default DescriptionBox
